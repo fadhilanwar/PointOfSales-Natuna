92d736c6ffc01d09c07708f6c1c06b172b382296
FathurrachmanMD <fathurrachmanmd@gmail.com>
2026-06-07 10:48:18 +0700

    Tambah Landing page

--- database/migrations/2026_04_19_123351_create_products_table.php
+++ database/migrations/2026_04_19_123351_create_products_table.php
@@ -14,10 +14,6 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('barcode')->unique()->nullable(); // Untuk scan POS
-<<<<<<< HEAD
-            $table->foreignId('category_id')->nullable()->constrained('categories')->onDelete('set null');            
-            $table->string('name');
-=======
             
             // Relasi kategori dari master
             $table->foreignId('category_id')->nullable()->constrained('categories')->onDelete('set null');
@@ -27,7 +23,6 @@ return new class extends Migration
             $table->string('slug')->unique();
             $table->text('description')->nullable();
             
->>>>>>> 57c1e8b0fcbe0f4cf13995a156c48274d702822d
             $table->decimal('cost_price', 12, 2); // Harga modal
             $table->decimal('base_price', 12, 2); // Harga jual
             $table->integer('stock')->default(0);
